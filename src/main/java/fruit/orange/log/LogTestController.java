e0923b70a5dda762c849983a36f1dd32e4bd7309
kwj <codenamezero001@gmail.com>
2024-04-02 23:46:52 +0900

    로그정보 초기 설정

--- src/main/java/fruit/orange/log/LogTestController.java
+++ src/main/java/fruit/orange/log/LogTestController.java
@@ -14,8 +14,13 @@ public class LogTestController {
 	public String logTest() {
 		String name = "Spring";
 		
-		System.out.println("name = " + name);
-		log.info(" info log={}", name);
+		//System.out.println("name = " + name);
+		
+		log.trace("trace log={}", name);	//
+		log.debug("debug log={}", name);	//디버그 할 때 보는 것(개발서버 같은 곳에서)
+		log.info(" info log={}", name);		//비즈니스 정보(운영시스템에서 봐야할 정보)
+		log.warn(" warn log={}", name);		//경고
+		log.error(" error log={}", name);	//이슈
 		
 		return "ok";
 	}
