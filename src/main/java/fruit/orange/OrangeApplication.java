f77be3e371511b5c0df35eefb7f8942a3a98cb37
kwj <codenamezero001@gmail.com>
2024-04-05 17:49:37 +0900

    Spring Boot 3.2 지원을 위한 MyBatis Spring 3.0.3 -SNAPSHOT 업데이트

--- src/main/java/fruit/orange/OrangeApplication.java
+++ src/main/java/fruit/orange/OrangeApplication.java
@@ -1,8 +1,10 @@
 package fruit.orange;
 
+//import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@MapperScan
 @SpringBootApplication
 public class OrangeApplication {
 
--- src/main/java/fruit/orange/repository/BoardRepository.java
+++ src/main/java/fruit/orange/repository/BoardRepository.java
@@ -10,10 +10,10 @@ import lombok.RequiredArgsConstructor;
 @Repository
 @RequiredArgsConstructor
 public class BoardRepository {
-	/*
+	
 	private final SqlSessionTemplate sql;
 	public void save(BoardDTO boardDTO) {
 		sql.insert("Board.save", boardDTO);
 	}
-	 */
+	 
 }
