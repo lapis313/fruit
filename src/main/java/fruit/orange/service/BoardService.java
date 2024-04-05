d8f25a2a4a5cfac315f73dfa89b117a4e8d5b9d2
kwj <codenamezero001@gmail.com>
2024-04-05 18:40:49 +0900

    application.yml DB설정 변경(로컬->AWS)

--- src/main/java/fruit/orange/service/BoardService.java
+++ src/main/java/fruit/orange/service/BoardService.java
@@ -12,6 +12,6 @@ public class BoardService {
 	private final BoardRepository boardRepository;
 
 	public void save(BoardDTO boardDTO) {
-		//boardRepository.save(boardDTO);		
+		boardRepository.save(boardDTO);		
 	}
 }
