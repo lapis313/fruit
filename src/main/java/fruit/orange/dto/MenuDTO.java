b2c30d0a5a788421a031526bcc2099eb8f6bc9b4
kwj <codenamezero001@gmail.com>
2024-04-09 12:54:31 +0900

    1. 게시판 -> 공지사항으로 변경 2. 메뉴 기본틀 구현-ING

--- src/main/java/fruit/orange/dto/MenuDTO.java
+++ src/main/java/fruit/orange/dto/MenuDTO.java
@@ -10,7 +10,10 @@ import lombok.ToString;
 public class MenuDTO {
 	private Long id;			//id
 	private Long parent_id;		//부모의 id
-	private String menu_name;	//메뉴명
+	private String menu_cd;		//메뉴코드
+	private String menu_name;	//메뉴이름
 	private String menu_url;	//주소
 	private Long list_order;	//정렬번호
+	private Long level;			//계층형 레벨
+	private String path;		//그룹
 }
