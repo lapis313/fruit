305004e1e30564453628b281159871a28b7af79a
kwj <codenamezero001@gmail.com>
2024-04-18 12:55:24 +0900

    js로드 테스트

--- src/main/java/fruit/orange/controller/MenuRestController.java
+++ src/main/java/fruit/orange/controller/MenuRestController.java
@@ -1,11 +1,14 @@
 package fruit.orange.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import fruit.orange.dto.MenuDTO;
 import fruit.orange.service.MenuService;
 import lombok.RequiredArgsConstructor;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -14,11 +17,19 @@ public class MenuRestController {
 
 	private final MenuService menuService;
 
+	@GetMapping("/list2")
+	public String findTree() {
+		String rtn = menuService.findTree();
+		System.out.println("test : "+rtn);
+		return rtn;
+	}
+	/**
 	@GetMapping(value="/list2", produces="application/json")
 	public String findTree(){
 		String rtn = menuService.findTree();
 		return rtn;
 	}
+	*/
 	/**
 	public String findTree() throws Exception {
 	//public String findTree(@RequestBody MenuDTO menuDTO) throws Exception {
